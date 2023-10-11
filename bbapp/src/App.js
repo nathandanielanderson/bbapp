1a7999a3ae863dcb7d248b226ee5c872cae02862
Nathan Anderson <nathandanielanderson@gmail.com>
2023-10-11 18:32:56 -0500

    created react app

---
+++ bbapp/src/App.js
@@ -0,0 +1,18 @@
+import transistor from './transistor.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+            <img src={transistor} className="App-transistor" alt="transistor" />
+        </p>
+              
+    </div>
+  );
+}
+
+
+
+export default App;
